82dc64ceda7648fd6dfe8c0d165dc0ec37f18a3e
Lex Greeven <lex.greeven@cern.ch>
2020-07-14 09:06:19 +0200

    Now taking sum, reformatted using black

--- solution.py
+++ solution.py
@@ -4,26 +4,25 @@ import os
 import sys
 import pandas as pd
 
+
 def predict(data):
-    """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)
-    TODO: modify it
-    """
-    result = data['col1'] - data['col2']
+    """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)"""
+    result = data["col1"] + data["col2"]
     return result
 
 
 def main(input_dir, output_dir):
     """ main procedure """
-    df = pd.read_csv(os.path.join(input_dir, 'data.tsv'), sep='\t')
+    df = pd.read_csv(os.path.join(input_dir, "data.tsv"), sep="\t")
 
     predicted_result = predict(df)
-    assert isinstance(predicted_result, pd.core.series.Series), \
-        f"Invalid predicted output type {type(predicted_result)}"
+    assert isinstance(
+        predicted_result, pd.core.series.Series
+    ), f"Invalid predicted output type {type(predicted_result)}"
 
     predicted_result.to_csv(
-        os.path.join(output_dir, 'data.predict'),
-        index=False,
-        header=False)
+        os.path.join(output_dir, "data.predict"), index=False, header=False
+    )
 
 
 if __name__ == "__main__":

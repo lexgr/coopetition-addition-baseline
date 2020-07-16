4ae51211325aeca0879a2b1213b6c73238fdde34
Lex Greeven <lex.greeven@cern.ch>
2020-07-16 09:40:30 +0200

    Push again to try coopetition

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 def predict(data):
-    """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)"""
+    """ Get two colums from `df` and returns the predicted ouput as a column (pd.Series)"""
     result = data["col1"] + data["col2"]
     return result
 
